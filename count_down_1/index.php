6c6c3bed68caea7cb0e4ea8e944723b509dcff87
Hirokazu Nakajima <hirokazu.dev@gmail.com>
2019-05-22 23:32:29 +0900

    count down

---
+++ count_down_1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script type="text/javascript" src="./js/script.js"></script>
+</head>
+<body>
+<ul>
+	<li id="day"></li>
+	<li id="hour"></li>
+	<li id="min"></li>
+	<li id="sec"></li>
+</ul>	
+</body>
+</html>
