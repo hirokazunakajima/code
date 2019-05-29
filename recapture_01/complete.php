b926878f5ad9fa693e8bf464a2a7b9ca57931317
Hirokazu Nakajima <hirokazu.dev@gmail.com>
2019-05-30 00:18:24 +0900

    reCapture v3

--- recapture_01/complete.php
+++ recapture_01/complete.php
@@ -6,14 +6,24 @@
 </head>
 <body>
 <?php
-	var_dump($_POST);
+	// var_dump($_POST);
     if(isset($_POST['g-recaptcha-token']) && !empty($_POST['g-recaptcha-token'])) {
         //your site secret key
         $secret = '6Lej6qUUAAAAAIQNivqKNljy3rAWK3XCTDEBQqdl';
         //get verify response data
         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-token']);
+
         $responseData = json_decode($verifyResponse);
-        var_dump($responseData);
+        /*
+        	var_dump($responseData);
+        	
+			object(stdClass)[1]
+			  public 'success' => boolean true
+			  public 'challenge_ts' => string '2019-05-29T13:46:44Z' (length=20)
+			  public 'hostname' => string 'hirokazunakajima.local' (length=22)
+			  public 'score' => float 0.9
+			  public 'action' => string 'test_action' (length=11)
+        */
 
         if(	$responseData->success ){
             echo("Working Fine");
