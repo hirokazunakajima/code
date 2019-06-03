2d6d2fe263e8ff430ca6506c96c141dded5352ce
Hirokazu Nakajima <hirokazu.dev@gmail.com>
2019-06-03 23:49:40 +0900

    count down

--- count_down_02/js/script.js
+++ count_down_02/js/script.js
@@ -8,7 +8,22 @@ var Countdown = {
 	sec:null,
 	init:function(){
 		var _self = this;
-		_self.timeToCountDown = new Date("December 31,2020 00:00:00");
+		var _now = new Date();
+		var _campDate = new Date("2019/06/04 00:00:00");
+		// console.log(_campDate);
+		if(_now < _campDate){
+			// start countdown
+			
+		}else{
+			console.log('too late to join to this campaing');
+			return; 
+		}
+
+		// var _d = new Date("2019/06/02 00:00:00");
+		// _campDate.setDate(_campDate.getDate() + 1);
+		// _campDate.setHours(10);
+
+		_self.timeToCountDown = _campDate;
 		_self.day = document.getElementById("day");
 		_self.hour = document.getElementById("hour");
 		_self.min = document.getElementById("min");
@@ -32,7 +47,7 @@ var Countdown = {
 			var min 	= Math.floor(((_self.timeToCountDown - currentTime)%(24*60*60*1000))/(60*1000))%60;
 			var sec 	= Math.floor(((_self.timeToCountDown - currentTime)%(24*60*60*1000))/1000)%60%60;
 			
-			day 		= ('0' + day).slice('-3');
+			day 		= ('0' + day).slice('-2');
 			hour 		= ('0' + hour).slice('-2');
 			min 		= ('0' + min).slice('-2');
 			sec 		= ('0' + sec).slice('-2');
